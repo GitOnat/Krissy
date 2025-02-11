c41f67c6e3b8f4aa81c701cf9f8803d6a430cd2d
GitOnat <maon0001@stud.kea.dk>
2025-02-11 23:51:24 +0100

    krissy images

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ document.getElementById('heart-button').addEventListener('click', function() {
         document.getElementById('main-message').style.opacity = 1;
         document.getElementById('sweet-message').style.opacity = 1;
         document.getElementById('final-message').style.opacity = 1;
-    }, 300); // Slight delay
+    }, 300);
 
     // Fade in images
     const images = document.querySelectorAll('.background-image');
     images.forEach((image, index) => {
         setTimeout(() => {
-            image.style.opacity = 0.6; // Make them visible
+            image.style.opacity = 1; // Make them fully visible
         }, 500 + (index * 200)); // Staggered effect
     });
 });
