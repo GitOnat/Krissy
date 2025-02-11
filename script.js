880771ffb0880e41f16b299c951e7f9de4d38a99
GitOnat <maon0001@stud.kea.dk>
2025-02-11 01:01:45 +0100

    krissy

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('heart-button').addEventListener('click', function() {
+    document.getElementById('message-container').style.display = 'block';
+    document.getElementById('heart-container').style.display = 'none';
+});
